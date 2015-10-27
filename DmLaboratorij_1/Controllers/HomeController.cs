21c4bc83e2f170e0e39d294c63109a704d33780b
Ivan Krešić <kresic_ivan@hotmail.com>
2015-10-27 11:55:00 +0100

    Test za commitanje

--- DmLaboratorij_1/Controllers/HomeController.cs
+++ DmLaboratorij_1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DmLaboratorij_1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descripti page.";
 
             return View();
         }
