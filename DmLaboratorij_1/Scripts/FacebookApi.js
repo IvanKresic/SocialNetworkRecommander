8bfdd7f91f29fadc1cd6ce01871dbc7570d0af7e
Ivan Krešić <kresic_ivan@hotmail.com>
2015-10-31 12:47:26 +0100

    Managed to get accessToken and UserID

--- DmLaboratorij_1/Scripts/FacebookApi.js
+++ DmLaboratorij_1/Scripts/FacebookApi.js
@@ -29,23 +29,26 @@ FB.login(function (response) {
 //    // handle the response
 //}, { scope: 'public_profile,email' });
 
-FB.getLoginStatus(function (response) {
-    if (response.status === 'connected') {
-        // the user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, a signed
-        // request, and the time the access token 
-        // and signed request each expire
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-        console.log(accessToken+"Here I ammmmm");
-    } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook, 
-        // but has not authenticated your app
-    } else {
-        // the user isn't logged in to Facebook.
-    }
-});
+function getLoginStatus() {
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token 
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+            var userID = response.authResponse.userID;
+            console.log(accessToken + "  " + userID);
+        } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook, 
+            // but has not authenticated your app
+        } else {
+            // the user isn't logged in to Facebook.
+        }
+    });
+}
 
 
 FB.api('/me', function (response) {
