9f7e68b19e4389f64bb929c4ed03413dc76f65ff
skatic7 <katicstanko@gmail.com>
2015-11-06 07:39:55 +0100

    riješen problem, dodana windows autorizacija

--- DmLaboratorij_1/Controllers/UserInfoController.cs
+++ DmLaboratorij_1/Controllers/UserInfoController.cs
@@ -12,7 +12,42 @@ using System.Runtime.Serialization.Json;
 namespace DmLaboratorij_1.Controllers
 {
     public class UserInfoController : ApiController
-    {/*
+    {
+
+        [Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+
+
+
+
+
+
+        /*
         private DM_DBEntities1 db = new DM_DBEntities1();
 
         [HttpPost]
