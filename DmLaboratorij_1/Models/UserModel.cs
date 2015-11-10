576b4b7faee4ffc08503dbceb8471f052250d587
3r1k0n <3r1k0n@gmail.com>
2015-11-10 12:52:58 +0100

    dodan UserModel.cs

---
+++ DmLaboratorij_1/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DmLaboratorij_1.Models
+{
+    public class UserModel
+    {
+        public object _id { get; set; } //MongoDb uses this field as identity.
+
+        public int Facebook_ID { get; set; }
+
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+
+        public string Email { get; set; }
+
+        public string DatumRodjenja { get; set; }
+
+        public string JSON_Objekt { get; set; }
+    }
+}
