785ad5f7183b38e92c22db1e72678d379d67a114
Ivan Krešić <kresic_ivan@hotmail.com>
2015-11-10 15:32:11 +0100

    Minor fix

--- DmLaboratorij_1/Controllers/UserInfoController.cs
+++ DmLaboratorij_1/Controllers/UserInfoController.cs
@@ -58,32 +58,5 @@ namespace DmLaboratorij_1.Controllers
         {
         }
 
-
-
-
-
-
-
-        /*
-        private DM_DBEntities1 db = new DM_DBEntities1();
-
-        [HttpPost]
-        public List<userInfo> Test(int data)
-        {
-            //var dataSet = JsonConvert.DeserializeObject(data);
-            //dataSet.ToString();
-            //string[] allData = data.Split('#');
-            //string userId = allData[0];
-            //string token = allData[1];
-            //userInfo userToken = db.userInfoes.Find(token);
-            List<userInfo> myList = new List<userInfo>();
-            //if(userToken != null)
-            //{
-            //    myList.Add(userToken);
-            //}
-            return myList;
-        }
-
-    }*/
     }
 }
